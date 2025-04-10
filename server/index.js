c002f75b5616d494dcc2dd85f04372de177c402e
bryon <bryonparis@gmail.com>
2025-04-10 10:22:06 -0700

    Config: Update CORS origin for Render deployment

--- server/index.js
+++ server/index.js
@@ -23,7 +23,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: [
+    'http://localhost:5173', 
+    'http://localhost:3000',
+    'https://findfoodcarts-frontend.onrender.com' // Example Render URL
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
